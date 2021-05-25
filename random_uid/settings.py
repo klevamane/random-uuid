3f3de9d75755d23381b21653a948974e11f1d0f2
Onengiye Onengiye <klevamane@gmail.com>
2021-05-25 16:42:45 +0100

    update the db connection

--- random_uid/settings.py
+++ random_uid/settings.py
@@ -84,24 +84,10 @@ WSGI_APPLICATION = 'random_uid.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        "NAME": "random",
-        "USER": "comfy",
-        # 'USER': 'user',
-        # 'PASSWORD': 'user',
-        "PASSWORD":"comfy",
-        "HOST": "localhost",
-        "PORT": 5432,
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
